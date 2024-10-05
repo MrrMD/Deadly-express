31b8fa5ba47b1c59f46008b42c165d5fc2f99196
MrrMD <52452257+MrrMD@users.noreply.github.com>
2024-10-05 15:20:33 +0300

    1

--- InventorySystem/InventoryItemMove.cs
+++ InventorySystem/InventoryItemMove.cs
@@ -1,6 +1,7 @@
 using InventorySystem;
 using Unity.Mathematics;
 using UnityEngine;
+using Utils;
 
 public static class InventoryItemMove 
 {
@@ -9,24 +10,24 @@ public static class InventoryItemMove
         var itemFrom = inventory.inventory[from];
         var itemTo = inventory.inventory[to];
 
-        if (inventory.inventory[from].Count == 0)
-        {
-            return;
-        }
+        if (inventory.inventory[from].Count == 0) return;
+        
 
-        if (inventory.inventory[to].ItemName == "Item")
+        if (itemTo.ItemName == "Item")
         {
             itemTo = new InventoryItem(inventory.inventory[from]);
             itemFrom.DecreaseStack(1);
             itemTo.Count = 1;
+            inventory.inventory[to] = itemTo;
+            inventory.inventory[from] = itemFrom;
         }
-        else if (inventory.inventory[from].ItemName == inventory.inventory[to].ItemName && inventory.inventory[to].CanStack(1))
+        else if (itemFrom.ItemName == itemTo.ItemName && itemTo.CanStack(1))
         {
             itemTo.IncreaseStack(1);
             itemFrom.DecreaseStack(1);
+            inventory.inventory[to] = itemTo;
+            inventory.inventory[from] = itemFrom;
         }
-        inventory.inventory[to] = itemTo;
-        inventory.inventory[from] = itemFrom;
     }
     public static void MoveItemStack(Inventory inventory, int from, int to)
     {
@@ -36,8 +37,8 @@ public static class InventoryItemMove
         if (itemTo.ItemName == "Item" || itemTo.ItemName != itemFrom.ItemName
                                       || (itemFrom.ItemName == itemTo.ItemName && !itemTo.CanStack(1)))
         {
-            inventory.inventory[from] = itemFrom;
-            inventory.inventory[to] = itemTo;
+            inventory.inventory[from] = itemTo;
+            inventory.inventory[to] = itemFrom;
             return;
         }
         
--- Utils/InventoryConstants.cs
+++ Utils/InventoryConstants.cs
@@ -1,7 +1,11 @@
-﻿namespace Utils
+﻿using System;
+
+namespace Utils
 {
     public class InventoryConstants
     {
         public static readonly int INVENTORY_SIZE = 6;
+        public static readonly String EMPTY_INVENTORY_ITEM_NAME = "Item";
+
     }
 }
