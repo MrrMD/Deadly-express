66855871586f68135d850399a292c052f53fa505
MrrMD <ironman.0945@gmail.com>
2024-10-21 23:41:12 +0300

    a
    a

--- Player/PlayerData.cs
+++ Player/PlayerData.cs
@@ -2,15 +2,17 @@
     using Player.RoleSystem;
     using UnityEngine;
 
+    
+    // Вынести все свойства в соответствующие компоненты
+    // Тут это как седло на ослике
+    
     namespace Player
     {
         public class PlayerData
         {
             public float Attack { get; } = 15f;
             public float AttackColdDown { get; } = 1f;
-            
             private float _repairSpeedRate;
-            
             public float AttackRadius { get; set; } = 1.5f;
             public float TrainStopRate { get; set; } = 1f;
             public float AnimalOpenSpeedRate { get; set; } = 1f;
--- Player/RoleSystem/Role.cs
+++ Player/RoleSystem/Role.cs
@@ -33,12 +33,6 @@ public abstract class Role : MonoBehaviour{
     }
 
     #endregion
-
-    public Role(string roleName, Sprite abilitySprite)
-    {
-        this.roleName = roleName;
-        this.abilitySprite = abilitySprite;
-    }
     
     internal abstract void EditPlayerData();
     internal abstract void AbilityActivate();
